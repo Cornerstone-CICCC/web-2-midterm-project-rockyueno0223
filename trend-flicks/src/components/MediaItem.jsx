c48c6b4125c454eb6dd41fe53ac1ca98cf3715a7
Hiroki Ueno <rockyueno@gmail.com>
2024-08-08 15:55:05 -0700

    updated media item style

--- trend-flicks/src/components/MediaItem.jsx
+++ trend-flicks/src/components/MediaItem.jsx
@@ -29,17 +29,22 @@ const MediaItem = ({ isOpen, onClose, media }) => {
   return (
     <div>
       <div id="modal-overlay" className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
-        <div className="bg-white rounded-md shadow-lg p-6 relative w-4/5 max-w-lg transform transition-all duration-300 scale-0 show-modal-animation">
+        <div className="bg-white rounded-md shadow-lg relative w-4/5 max-w-lg pb-4 transform transition-all duration-300 scale-0 show-modal-animation">
           <button
             onClick={onClose}
-            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+            className="absolute top-2 right-3 text-4xl text-white hover:text-gray-700"
           >
             &times;
           </button>
-          <h2 className="text-xl font-bold">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${media.backdrop_path}`}
+            alt={media.media_type === 'movie' ? media.title : media.name}
+            className='w-full h-auto'
+          />
+          <h2 className="text-xl font-bold mt-4 mb-2">
             {isMovie ? media.title : media.name}
           </h2>
-          <div className='w-full flex justify-between px-4'>
+          <div className='w-full flex justify-between px-6 text-gray-600'>
             <span>
               {isMovie ? media.release_date.substring(0, 4) : media.first_air_date.substring(0, 4)}
             </span>
@@ -47,8 +52,8 @@ const MediaItem = ({ isOpen, onClose, media }) => {
               {media.media_type}
             </span>
           </div>
-          <p>{media.overview ? media.overview : "No text available"}</p>
-          <div className='w-full flex justify-between px-4'>
+          <p className='text-left mt-2 mb-3 px-4'>{media.overview ? media.overview : "No text available"}</p>
+          <div className='w-full flex justify-between px-6 text-gray-600'>
             <span>
               <Rating name="rating" value={roundedRating} precision={0.1} readOnly />
             </span>
