c0f0b3d4bd3927ed7862afb21f189922b551a590
Hiroki Ueno <rockyueno@gmail.com>
2024-08-08 14:23:05 -0700

    created media item

--- trend-flicks/src/components/MediaItem.jsx
+++ trend-flicks/src/components/MediaItem.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import "../css/modal.css";
 
-const MediaItem = () => {
+const MediaItem = ({ isOpen, onClose, media }) => {
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (event.target.id === 'modal-overlay') {
+        onClose();
+      }
+    };
+
+    document.addEventListener('click', handleOutsideClick);
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, [onClose]);
+
+  if (!isOpen) return null;
+
+  const isMovie = media.media_type === "movie";
+
   return (
     <div>
-      Media Item
+      <div id="modal-overlay" className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
+        <div className="bg-white rounded-md shadow-lg p-6 relative w-1/3 max-w-lg transform transition-all duration-300 scale-0 show-modal-animation">
+          <button
+            onClick={onClose}
+            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          >
+            &times;
+          </button>
+          <h2 className="text-xl font-bold">
+            {isMovie ? media.title : media.name}
+          </h2>
+          <p>{media.overview ? media.overview : "No text available"}</p>
+        </div>
+      </div>
     </div>
   )
 }
--- trend-flicks/src/components/MediaThumbnail.jsx
+++ trend-flicks/src/components/MediaThumbnail.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import MediaItem from './MediaItem';
 
-const MediaThumbnail = ({media}) => {
+const MediaThumbnail = ({ media }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
   return (
     <div className='flex-shrink-0 w-full max-w-[500px] sm:w-1/2 md:w-[300px]'>
       <img
         src={`https://image.tmdb.org/t/p/w500${media.poster_path}`}
         alt={media.media_type === 'movie' ? media.title : media.name}
         className='w-full h-auto rounded-md cursor-pointer'
+        onClick={openModal}
       />
+      <MediaItem isOpen={isModalOpen} onClose={closeModal} media={media} />
     </div>
   )
 }
