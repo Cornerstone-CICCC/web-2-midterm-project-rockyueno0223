6ab631a9b65d04e02f32c27086aa7c078ec0c0dc
Hiroki Ueno <rockyueno@gmail.com>
2024-08-08 13:25:04 -0700

    updated responsive style

--- trend-flicks/src/components/MediaThumbnail.jsx
+++ trend-flicks/src/components/MediaThumbnail.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const MediaThumbnail = ({media}) => {
   return (
-    <div className='flex-shrink-0 w-full sm:w-1/2 bg-gray-200'>
+    <div className='flex-shrink-0 w-full max-w-[500px] sm:w-1/2 md:w-[300px]'>
       <img
-        src={`https://image.tmdb.org/t/p/w500${media.backdrop_path}`}
+        src={`https://image.tmdb.org/t/p/w500${media.poster_path}`}
         alt={media.media_type === 'movie' ? media.title : media.name}
         className='w-full h-auto rounded-md cursor-pointer'
       />
